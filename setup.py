8b291d59b418bed52db031298278e08be02a065d
Psycho <psycho@Psychos-Laptop.local>
2025-09-15 22:32:50 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'wasteDetection',
+    version= '0.0.0',
+    author= 'Jitendra Tamang',
+    author_email= 'jitendratamang00@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
