060e940b73e65dc67fa6a25c76c790d317dc9644
Psycho <psycho@Psychos-Laptop.local>
2025-09-16 03:02:54 +0545

    Data Ingestion Added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Waste-Detection
+
+## Workflows
+
+1. Constants
+2. Entity
+3. Components
+4. Pipelines
+5. app.py
---
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -0,0 +1,12 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1zi1I56ubsZ8yynxyMtHz0z__Bl5KR-3t/view?usp=drive_link"
+
