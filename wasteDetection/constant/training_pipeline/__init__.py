8c0ff590e13b607f9121ca602d23ce79b37f4484
Psycho <psycho@Psychos-Laptop.local>
2025-09-16 03:46:32 +0545

    Data Validation Added

--- wasteDetection/constant/training_pipeline/__init__.py
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -10,3 +10,12 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1zi1I56ubsZ8yynxyMtHz0z__Bl5KR-3t/view?usp=drive_link"
 
+"""
+Data Validation related contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
